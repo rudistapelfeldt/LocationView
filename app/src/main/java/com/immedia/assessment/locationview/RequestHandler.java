6e7101c93bb9bf16befcc46a6826056521db79ef
rudistapelfeldt <r.stapelfeldt@gmail.com>
2016-06-08 20:06:34 +0200

    another paranoid commit. up to getting json response from testUrl

--- app/src/main/java/com/immedia/assessment/locationview/RequestHandler.java
+++ app/src/main/java/com/immedia/assessment/locationview/RequestHandler.java
@@ -7,6 +7,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,29 +17,38 @@ import org.json.JSONObject;
 public class RequestHandler {
 
     public static void requestInstagram(double lat, double lng){
-        //String url = "https://api.instagram.com/v1/media/search?lat=" + lat + "&lng=" + lng + "&access_token=" + ApplicationData.getAuthToken();
-        String testUrl = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + ApplicationData.getAuthToken();
-        Log.i("Login", testUrl);
-        JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                (Request.Method.GET , testUrl, null, new Response.Listener<JSONObject>() {
+        if(ApplicationData.getAuthToken() != null) {
+            //String url = "https://api.instagram.com/v1/media/search?lat=" + lat + "&lng=" + lng + "&access_token=" + ApplicationData.getAuthToken();
+            String testUrl = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + ApplicationData.getAuthToken();
+            Log.i("Login", testUrl);
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                    (Request.Method.GET, testUrl, null, new Response.Listener<JSONObject>() {
 
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONObject data = response;
-                            Log.i("LocationView", data.getString("location"));
-                        }catch(JSONException je){
-                            Log.i("LocationView", "JSONException: " +je.getMessage());
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            //Log.i("LocationView", "Response = "+ response.toString());
+                            try {
+                                JSONObject jsonObject = response;
+                                JSONArray jsonArray = jsonObject.getJSONArray("data");
+                                for (int index = 0; index < jsonArray.length(); index++) {
+                                    JSONObject dataId = jsonArray.getJSONObject(index).getJSONObject("location");
+                                    String place = dataId.getString("name");
+                                    Log.i("LocationView", "@@@@@ place from json = " + place);
+                                }
+                                //Log.i("LocationView", "location from JSON response: " + place);
+                            } catch (JSONException je) {
+                                Log.i("LocationView", "JSONException: " + je.getMessage());
+                            }
                         }
-                    }
-                }, new Response.ErrorListener() {
+                    }, new Response.ErrorListener() {
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO Auto-generated method stub
-                        Log.i("Login", "Login ERROR" + error.getMessage());
-                    }
-                });
-        AppController.getInstance().addToRequestQueue(jsObjRequest);
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // TODO Auto-generated method stub
+                            Log.i("Login", "Login ERROR" + error.getMessage());
+                        }
+                    });
+            AppController.getInstance().addToRequestQueue(jsObjRequest);
+        }
     }
 }
